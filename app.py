775ee2bcb2a07b6eaa19fd1cac56ed33505953b2
Alexandru Tudorica <tudalex@gmail.com>
2019-04-19 23:59:06 +0300

    Initial commit.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
